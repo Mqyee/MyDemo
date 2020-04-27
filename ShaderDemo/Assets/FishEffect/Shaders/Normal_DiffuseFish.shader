3d15bc280833e8c0c019fdcb18dcaf2e223bdbc9
Qyee <779745707@qq,com>
2020-04-27 21:53:57 +0800

    add

---
+++ ShaderDemo/Assets/FishEffect/Shaders/Normal_DiffuseFish.shader
@@ -0,0 +1,158 @@
+﻿Shader "Actor/Character/Fish_Shake"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_Speed("摆动速度",Range(0,50)) = 0
+		_MoveOffset("摆动偏移",Range(0,10)) = 0
+		_Frequence("摆动频率",Range(0,50)) = 0
+		_HeadLength("头部固定长度",float) = 0
+		_HeadRotate("头部旋转角度",range(0,50)) = 0
+		[Toggle(_ROTATE_BODY)]_Rotate0("脊椎旋转",float) = 0
+		_RoaScale("脊椎旋转幅度",Range(0,2)) = 0
+		_RoaFrequence("脊椎旋转频率",Range(0,50)) = 0
+		[Toggle(_ROTATE_LEFT_RIGHT)]_Rotate1("左右摆动",float) = 1
+		[Toggle(_ROTATE_UP_DOWN)]_Rotate2("上下摆动",float) = 0
+		[Toggle(_SHOW_INNER_EFFECT)]_ShowInnerEffect("内发光",float) = 0
+		[HDR] _Color ("Color", Color) = (1, 1, 1, 1)
+		[HDR]_InnerColor("InnerColor",Color) = (1,1,1,1)
+		_InnerPower("InnerPower",Range(1,10)) = 8
+		_IsShowInner("IsShowInner",Range(0,1)) = 0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" "Queue"="Transparent-10"}
+		
+		LOD 200
+
+		Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+			#pragma multi_compile_instancing
+			#pragma multi_compile __ _ROTATE_BODY
+			#pragma multi_compile __ _ROTATE_LEFT_RIGHT
+			#pragma multi_compile __ _ROTATE_UP_DOWN
+			#pragma multi_compile __ FOG_LINEAR	
+			#pragma multi_compile	__ _SHOW_INNER_EFFECT
+            #include "UnityCG.cginc"
+			#include "Lighting.cginc"
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 pos : SV_POSITION;
+				float3 worldPos : TEXCOORD1;
+				float3 normal : TEXCOORD2;
+				UNITY_FOG_COORDS(3)
+
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+			sampler2D _NormalMap;
+			float _RoaScale;
+			float _RoaFrequence;
+			float _MoveOffset;
+			float _HeadLength;
+			float _HeadRotate;
+			float4 _Color;
+			float4 _InnerColor;
+			half _InnerPower;
+			UNITY_INSTANCING_BUFFER_START(Props)
+				UNITY_DEFINE_INSTANCED_PROP(float, _Speed)
+				UNITY_DEFINE_INSTANCED_PROP(float, _Frequence)
+				UNITY_DEFINE_INSTANCED_PROP(float,_IsShowInner)
+			UNITY_INSTANCING_BUFFER_END(Props)
+
+			float3x3 AngleAxis3x3(float angle, float3 axis)
+			{
+				float c, s;
+				sincos(angle, s, c);
+ 
+				float t = 1 - c;
+				float x = axis.x;
+				float y = axis.y;
+				float z = axis.z;
+ 
+				return float3x3(
+					t * x * x + c, t * x * y - s * z, t * x * z + s * y,
+					t * x * y + s * z, t * y * y + c, t * y * z - s * x,
+					t * x * z - s * y, t * y * z + s * x, t * z * z + c
+				);
+			}
+
+			float4 vertexmove(float3 vertex)
+			{
+				float time = _Time.y * UNITY_ACCESS_INSTANCED_PROP(Props,_Speed);
+
+				//脊椎旋转
+				#ifdef _ROTATE_BODY
+					float warp = sin(time + vertex.x *_RoaFrequence) * _RoaScale ;
+					float3x3 roaMat = AngleAxis3x3(warp,float3(1,0,0));
+					vertex = lerp(vertex,mul(roaMat,vertex),1);
+				#endif
+
+				//左右or上下摆动
+				float move_offset = cos( time) * _MoveOffset;
+				float headfixed = smoothstep(vertex.x,vertex.x + _HeadLength,_HeadRotate);
+				float warp1 = sin(time + vertex.x *UNITY_ACCESS_INSTANCED_PROP(Props,_Frequence));
+				#ifdef _ROTATE_LEFT_RIGHT
+					float3x3 roaMat1 = AngleAxis3x3(warp1,float3(0,0,1));
+					vertex.y -= move_offset; 
+				#else
+					float3x3 roaMat1 = AngleAxis3x3(warp1,float3(0,1,0));
+					vertex.z -= move_offset; 
+				#endif
+				vertex = lerp(vertex,mul(roaMat1,vertex),headfixed);
+
+				return float4(vertex,1.0);
+			}
+
+            v2f vert (appdata_base v)
+            {
+                v2f o;
+				UNITY_INITIALIZE_OUTPUT(v2f,o);
+				UNITY_SETUP_INSTANCE_ID(v);
+                UNITY_TRANSFER_INSTANCE_ID(v, o);
+
+				v.vertex = vertexmove(v.vertex);
+                o.pos = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.texcoord, _MainTex);
+				o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+				o.normal = UnityObjectToWorldNormal(v.normal);
+				UNITY_TRANSFER_FOG(o, o.pos);
+
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				fixed4 col = tex2D(_MainTex,i.uv);
+				half3 viewDir = normalize(UnityWorldSpaceViewDir(i.worldPos));
+			#ifdef _SHOW_INNER_EFFECT
+				half nv = saturate(dot(i.normal,viewDir));
+				half3 innerCol = _InnerColor.rgb *  pow((1- nv)*UNITY_ACCESS_INSTANCED_PROP(Props,_IsShowInner),_InnerPower) * (cos(_Time.z*2 ) + 1);
+				col.rgb += innerCol;
+			#else
+				col = col * _Color;
+			#endif
+			#ifndef USING_DIRECTIONAL_LIGHT
+				half3 lightDir = normalize(UnityWorldSpaceLightDir(i.worldPos));
+			#else
+				half3 lightDir = _WorldSpaceLightPos0.xyz;
+			#endif
+
+
+				 return col;
+            }
+            ENDCG
+        }
+    }
+}
+
+			
---
+++ ShaderDemo/Assets/WaterEffect/Scripts/PlaneReflection.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Renderer))]
+public class PlaneReflection : MonoBehaviour
+{    
+    public LayerMask reflectionMask = 0;
+    public CameraClearFlags clearFlags = CameraClearFlags.Color;
+    public Color clearColor = new Color(0.5f, 0.75f, 1.0f, 1.0f);
+    public float clipPlaneOffset = 0.05f;
+    public bool refreshReflection = false;
+    public RenderingPath path = RenderingPath.Forward;
+
+    private Camera reflectCamera = null;
+    private Vector4 clipPlane = Vector4.zero;
+    //private int _ReflectionTex = -1;
+    //private int _Reflection = -1;
+    private readonly string _ReflectionTex = "_ReflectionTex";
+    private readonly string _Reflection = "_Reflection";
+
+    private int dropLayer = 0;
+    private List<string> dropLayerNames;
+    private Material waterMaterial;
+    private bool renderReflection = true;
+    private uint sceneID = 0;
+
+    private void Awake()
+    {
+        //dropLayerNames = new List<string>
+        //{
+        //    LAYER.WATER,
+        //    LAYER.UI_HERO,
+        //    LAYER.HERO,
+        //    LAYER.PLAYER,
+        //    LAYER.NPC,
+        //    LAYER.MONSTER,
+        //    LAYER.Hair,
+        //    LAYER.Hero_Hair,
+        //    LAYER.Player_Effect,
+        //    LAYER.FOLLOWER,
+        //    LAYER.Grab,
+        //    LAYER.PET
+        //};
+
+        dropLayer = LayerMask.GetMask(dropLayerNames.ToArray());
+
+        gameObject.layer = LayerMask.NameToLayer("Water");
+        reflectionMask = ~dropLayer;
+
+        //Messenger.AddListener<object, object>(MSG_DEFINE.MSG_QUALITY_SETTING_CHANGE, QualitySettingChange);
+    }
+
+    private void Start()
+    {
+        waterMaterial = GetComponent<Renderer>().sharedMaterial;
+        if (waterMaterial == null)
+            enabled = false;
+    }
+
+    void OnEnable()
+    {
+        dropLayer = LayerMask.GetMask(dropLayerNames.ToArray());
+
+        //暂时用名字直接SetTexture，免得多个水面反射出现bug
+        //_ReflectionTex = Shader.PropertyToID("_ReflectionTex");
+        //_Reflection = Shader.PropertyToID("_Reflection");
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        UnityEditor.SceneView.RepaintAll();
+    }
+#endif
+
+    private void OnDisable()
+    {
+        Destroy();
+    }
+
+    void OnApplicationQuit()
+    {
+        Destroy();
+    }
+
+    private void OnDestroy()
+    {
+        Destroy();
+        //Messenger.RemoveListener<object, object>(MSG_DEFINE.MSG_QUALITY_SETTING_CHANGE, QualitySettingChange);
+    }
+
+    private bool CheckSupport()
+    {
+        bool isSupport = true;
+
+        if (waterMaterial == null || waterMaterial.shader == null)
+            isSupport = false;
+
+        return isSupport;
+    }
+
+    private void Destroy()
+    {
+        if (reflectCamera != null)
+        {
+            RenderTexture rt = reflectCamera.targetTexture;
+            reflectCamera.targetTexture = null;
+            if(waterMaterial != null && waterMaterial.HasProperty(_ReflectionTex))
+                waterMaterial.SetTexture(_ReflectionTex, null);
+            rt.Release();
+            DestroyImmediate(rt);
+#if UNITY_EDITOR
+            DestroyImmediate(reflectCamera.gameObject);
+            Caching.ClearCache();
+#else
+            Destroy(reflectCamera.gameObject);
+#endif
+            reflectCamera = null;
+            rt = null;
+        }
+    }
+
+    void CreateReflectCamera(Camera camera)
+    {
+        if (reflectCamera != null) return;
+
+        string str = gameObject.name + "Reflection" + camera.name;
+        GameObject go = new GameObject(str, typeof(Camera))
+        {
+            hideFlags = HideFlags.DontSave | HideFlags.NotEditable
+        };
+
+        go.transform.parent = transform;
+
+        reflectCamera = go.GetComponent<Camera>();
+        reflectCamera.depthTextureMode = DepthTextureMode.None;
+        reflectCamera.renderingPath = RenderingPath.Forward;
+        reflectCamera.allowHDR = false;
+        reflectCamera.allowMSAA = false;
+        reflectCamera.useOcclusionCulling = false;
+        reflectCamera.backgroundColor = clearColor;
+        reflectCamera.clearFlags = clearFlags;
+        reflectCamera.cullingMask = reflectionMask & ~LayerMask.GetMask("Water", "UI");
+        reflectCamera.useOcclusionCulling = false;
+        reflectCamera.enabled = false;
+
+        reflectCamera.farClipPlane = camera.farClipPlane;
+        reflectCamera.nearClipPlane = camera.nearClipPlane;
+        reflectCamera.orthographic = camera.orthographic;
+        reflectCamera.fieldOfView = camera.fieldOfView;
+        reflectCamera.aspect = camera.aspect;
+        reflectCamera.orthographicSize = camera.orthographicSize;
+    }
+
+    void CreateRenderTexture(Camera camera)
+    {
+        if (!reflectCamera.targetTexture)
+        {
+            RenderTextureFormat format = RenderTextureFormat.Default;
+
+            if (SystemInfo.SupportsRenderTextureFormat(RenderTextureFormat.RGB111110Float))
+                format = RenderTextureFormat.RGB111110Float;
+
+            RenderTexture rt = new RenderTexture(camera.pixelWidth, camera.pixelHeight, 16, format)
+            {
+                name = "_ReflectionTex",
+                hideFlags = HideFlags.DontSave
+            };
+            rt.Create();
+            reflectCamera.targetTexture = rt;            
+        }
+    }
+
+    void RenderReflection()
+    {
+        GL.invertCulling = true;
+        Vector3 n = transform.up;
+        float d = -Vector3.Dot(n, transform.position) - clipPlaneOffset;
+        Matrix4x4 reflectMatrix = CalculateReflectionMatrix(n, d);
+        reflectCamera.worldToCameraMatrix = Camera.current.worldToCameraMatrix * reflectMatrix;
+        clipPlane.Set(n.x, n.y, n.z, d);
+        clipPlane = reflectCamera.cameraToWorldMatrix.transpose * clipPlane;
+        reflectCamera.projectionMatrix = reflectCamera.CalculateObliqueMatrix(clipPlane);
+        reflectCamera.clearFlags = clearFlags;
+        reflectCamera.renderingPath = path;
+        reflectCamera.cullingMask = reflectionMask & ~dropLayer;
+        reflectCamera.Render();
+        GL.invertCulling = false;
+    }
+
+    private void OnWillRenderObject()
+    {
+        if (waterMaterial == null)
+            return;
+
+        if (refreshReflection)
+        {
+            Destroy();
+            refreshReflection = false;
+        }
+
+        //if(SceneMgr.Instance != null)
+        //    sceneID = SceneMgr.Instance.SceneId;
+
+        //美术制作过程中根据材质球控制是否开启实时反射
+//#if UNITY_EDITOR
+        if (waterMaterial.HasProperty(_Reflection))
+        {
+            renderReflection = waterMaterial.GetFloat(_Reflection) == 1 ? true : false;
+        }
+        else
+        {
+            renderReflection = false;
+
+            if(Application.isPlaying)
+            {
+                enabled = false;
+            }
+        }
+//#endif
+
+        if (renderReflection)
+        {
+            CreateReflectCamera(Camera.current);
+            CreateRenderTexture(Camera.current);
+
+            if (reflectCamera != null && CheckSupport() && reflectCamera.targetTexture != null)
+            {
+                RenderReflection();
+                if(waterMaterial.HasProperty(_Reflection))
+                    waterMaterial.SetFloat(_Reflection, 1);
+                if(waterMaterial.HasProperty(_ReflectionTex))
+                    waterMaterial.SetTexture(_ReflectionTex, reflectCamera.targetTexture);
+            }
+        }
+        else
+        {
+            if (waterMaterial.HasProperty(_Reflection))
+                if(waterMaterial.GetFloat(_Reflection) != 0)
+                    waterMaterial.SetFloat(_Reflection, 0);
+            Destroy();
+        }
+    }
+
+    private void QualitySettingChange(object lastVal, object curVal)
+    {
+        if (!CheckSupport()) return;
+
+        int curQSVal = Convert.ToInt32(curVal);
+
+        //dataconfig.QualitySetting curQSInfo = QualitySettingMgr.GetQualitySetInfo(curQSVal);
+
+        //water开到2级才有实时反射
+        //shaderlod 400 实时反射 折射 高光
+        //shaderlod 300 cube反射 高光
+        //shaderlod 200 cube反射
+        //目前僅家園的水執行此劃分，其他場景無最高品質實時反射
+        //if (!curQSInfo.Invalid() && (sceneID == 221101 || sceneID == 221100 || sceneID == 221104))
+        //    renderReflection = curQSInfo.water == 2 ? true : false;
+        //else
+        //    renderReflection = false;
+
+        //实时反射全部关闭
+        renderReflection = false;
+    }
+
+    Matrix4x4 CalculateReflectionMatrix(Vector3 n, float d)
+    {
+        Matrix4x4 reflectionMat;
+        reflectionMat.m00 = (1f - 2f * n.x * n.x);
+        reflectionMat.m01 = (-2f * n.x * n.y);
+        reflectionMat.m02 = (-2f * n.x * n.z);
+        reflectionMat.m03 = (-2f * d * n.x);
+
+        reflectionMat.m10 = (-2f * n.y * n.x);
+        reflectionMat.m11 = (1f - 2f * n.y * n.y);
+        reflectionMat.m12 = (-2f * n.y * n.z);
+        reflectionMat.m13 = (-2f * d * n.y);
+
+        reflectionMat.m20 = (-2f * n.z * n.x);
+        reflectionMat.m21 = (-2f * n.z * n.y);
+        reflectionMat.m22 = (1f - 2f * n.z * n.z);
+        reflectionMat.m23 = (-2f * d * n.z);
+
+        reflectionMat.m30 = 0;
+        reflectionMat.m31 = 0;
+        reflectionMat.m32 = 0;
+        reflectionMat.m33 = 1;
+
+        return reflectionMat;
+    }
+}
